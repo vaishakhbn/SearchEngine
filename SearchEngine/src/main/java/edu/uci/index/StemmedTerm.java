674b83fbde0c34515f031f708f3927cc8f248e37
spethe <swanand.pethe@gmail.com>
2015-02-10 18:10:53 -0800

    Stemmed Term Object commit.

---
+++ SearchEngine/src/main/java/edu/uci/index/StemmedTerm.java
@@ -0,0 +1,43 @@
+package edu.uci.index;
+
+import java.util.List;
+
+/**
+ * Created by swanand on 2/10/2015.
+ */
+public class StemmedTerm {
+    private String docId;
+    private String stem;
+    private List<Integer> positions;
+    private int frequency;
+
+    public StemmedTerm(String docId, String stem) {
+        this.docId = docId;
+        this.stem = stem;
+    }
+
+    public List<Integer> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<Integer> positions) {
+        this.positions = positions;
+    }
+
+    public String getDocId() {
+        return docId;
+    }
+
+    public String getStem() {
+        return stem;
+    }
+
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+}
